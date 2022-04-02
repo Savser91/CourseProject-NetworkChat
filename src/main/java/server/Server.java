2097fa09b363d4d6826a4bd4f39877673211a0be
Savser91 <savser91@gmail.com>
2022-04-02 20:30:24 +0300

    First commit. Begin writing server

---
+++ src/main/java/server/Server.java
@@ -0,0 +1,40 @@
+package server;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Server {
+    // порт, прослушивающий сервер
+    private static final int PORT = 65500;
+    // лист, в который будут добавляться клиенты, подключившиеся к серверу
+    private ArrayList<ClientHandler> clients = new ArrayList<>();
+
+    public Server() {
+        Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Сервер запущен");
+            while (true) {
+                clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler();
+                clients.add(client);
+                new Thread(client).start();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+                System.out.println("Сервер остановлен");
+                serverSocket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+}
