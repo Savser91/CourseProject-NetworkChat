cbe87bcd5c68ebe990028c30f0c0bf5ae0be1a88
Savser91 <savser91@gmail.com>
2022-04-02 22:15:35 +0300

    Client added

---
+++ src/main/java/MainClient.java
@@ -0,0 +1,8 @@
+import client.Client;
+
+public class MainClient {
+    public static void main(String[] args) {
+        Client client1 = new Client();
+        Client client2 = new Client();
+    }
+}
---
+++ src/main/java/MainServer.java
@@ -0,0 +1,7 @@
+import server.Server;
+
+public class MainServer {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
